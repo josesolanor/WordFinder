056b31dbf3eee2ab7549395b7efcdabb07c909ed
Jose Solano R <josesolano@outlook.com>
2021-07-27 23:55:28 -0400

    Add Initial values and read json file

--- WordFinder/Console/Program.cs
+++ WordFinder/Console/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Application;
+using Domain;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
-namespace Console
+namespace ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\..\\Sources\\DataSource.json");
+            var jsonString = File.ReadAllText(path);
+            var sourceObject = JObject.Parse(jsonString);
+
+            Dictionary dictionary  = new Dictionary(sourceObject.SelectToken("Dictionary").Values<string>().ToList());
+            Matrix matrix = new Matrix(sourceObject.SelectToken("Matrix").Values<string>().ToList());
+            var result = new WordFinder(dictionary.Source).Find(matrix.Source);
+            Console.WriteLine(string.Join(",", result));
         }
     }
 }
