cf943f6295dc8b1f2b2740de1cd191ec5d2be392
Jose Solano R <josesolano@outlook.com>
2021-07-27 22:18:13 -0400

    Add initial methods and variables

--- WordFinder/Application/WordFinder.cs
+++ WordFinder/Application/WordFinder.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Application
 {
     public class WordFinder
     {
+        private readonly HashSet<string> _matrix;
+        public WordFinder(IEnumerable<string> matrix)
+        {
+            _matrix = new HashSet<string>(matrix);
+        }
+        public IEnumerable<string> Find(IEnumerable<string> wordstream)
+        {
+            var result = new HashSet<string>();
 
+            return result.ToList();
+        }
     }
 }
